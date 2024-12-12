a00cfafdb2f123122ee4cefc56dd49582abffd6a
surya06122002 <132427252+surya06122002@users.noreply.github.com>
2024-12-12 17:54:53 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: "https://tm-main-frontend.onrender.com",
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
